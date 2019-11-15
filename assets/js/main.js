e561d7edeb0b43735339533a4e6e613ac530687e
rathanont <rathanont.s@gmail.com>
2019-11-15 17:46:28 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,32 @@ $(document).ready(function () {
   $('[data-toggle="menu"]').on('click', function () {
     $(this).toggleClass('active');
     $('.nav-collapse').toggleClass('open');
-  })
+  });
+  $('.nav-menu .nav-link').on('click', function () {
+    $('[data-toggle="menu"]').removeClass('active');
+    $('.nav-collapse').removeClass('open');
+  });
 
   $('.carousel').carousel({
     interval: 2000,
     pause: false,
     touch: false
-  })
+  });
 
-  var mySwiper = new Swiper('.swiper-container', {
-    // initialSlide: 5,
+  $("#sec5 .slick-for").slick({
+    asNavFor: '.slick-nav',
+    prevArrow: '<button type="button" class="slick-prev"><img src="./assets/images/icons/arrow-prev.svg" /></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="./assets/images/icons/arrow-next.svg" /></button>'
+  });
+  $("#sec5 .slick-nav").slick({
+    slidesToShow: 7,
+    asNavFor: '.slick-for',
+    focusOnSelect: true
+  });
+
+  var swiper = new Swiper('#main', {
+    // initialSlide: 4,
+    hashNavigation: true,
     direction: 'vertical',
     mousewheel: true,
     keyboard: true,
@@ -35,6 +51,22 @@ $(document).ready(function () {
       },
     }
   });
-
-  $('.toggle-icon').html('<svg xmlns="http://www.w3.org/2000/svg" width="31" height="20" viewBox="0 0 31 20"><g transform="translate(-264 -29)"><rect width="31" height="4" transform="translate(264 29)" fill="#50bd96"/><rect width="31" height="4" transform="translate(264 37)" fill="#50bd96"/><rect width="31" height="4" transform="translate(264 45)" fill="#50bd96"/></g></svg>')
+  $('#slide1').click(swiper, function () {
+    swiper.slideTo(0);
+  })
+  $('#slide2').click(swiper, function () {
+    swiper.slideTo(1);
+  })
+  $('#slide3').click(swiper, function () {
+    swiper.slideTo(2);
+  })
+  $('#slide4').click(swiper, function () {
+    swiper.slideTo(3);
+  })
+  $('#slide5').click(swiper, function () {
+    swiper.slideTo(4);
+  })
+  $('#slide6').click(swiper, function () {
+    swiper.slideTo(5);
+  })
 });
